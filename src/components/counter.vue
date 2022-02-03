01331095aae0191befb3af9c7a11efbab8d888bd
saurab10 <sauravsilwal45@gmail.com>
2022-02-03 18:04:20 +0545

    initial commit

---
+++ src/components/counter.vue
@@ -0,0 +1,18 @@
+<template>
+<h1 class="title">Welcome to Counter Application</h1>
+<div class="counter-box">
+<div class="box">
+    <h1>Counter: {{count}} </h1>
+</div>
+
+</template>
+<script>
+
+export default 
+    {
+        name:'Counter',
+    }
+</script>
+<style>
+
+</style>
