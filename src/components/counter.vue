5400461b748968de282515e93f5c9190ff68ce93
saurab10 <sauravsilwal45@gmail.com>
2022-02-03 18:07:10 +0545

    feat: Increment, reset , decrement button created

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -4,7 +4,12 @@
 <div class="box">
     <h1>Counter: {{count}} </h1>
 </div>
-
+<div id ="counter-app">
+<button class="button-1" @click="increment">Increment</button>
+<button class="button-2" @click="reset">Reset</button>
+<button class="button-3" @click="decrement">Decrement</button>
+</div>
+</div>
 </template>
 <script>
 
